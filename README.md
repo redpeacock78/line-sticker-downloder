8ec1074f95f3aa706ec43041dd5e3ad79952aafc
redpeacock78 <scarletpeacock78@gmail.com>
2020-08-03 21:40:17 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,267 @@
+## line-sticker
+### Install
+```bash
+$ npm install -g line-sticker
+             or
+$ yarn global add line-sticker
+```
+or
+```bash
+$ git clone https://github.com/redpeacock78/line-sticker
+$ cd line-sticker/
+$ npm install
+      or
+$ yarn install
+$ tsc src/*.ts --outDir dist/
+$ node install -g
+      or
+$ yarn global add
+```
+### Usage
+```bash
+$ line-sticker -h
+Usage: main line-sticker [options] [sticker_id]
+
+Options:
+  -V, --version    output the version number
+  -a, --animation  With animation stickers (APNG)
+  -g, --gif        With animation stickers (GIF)
+  -s, --sound      With sound(animation) stickers sound (m4a)
+  -d --dir <dir>   Specify the directory where you want to store the data
+  -h, --help       display help for command
+
+$ line-sticker -a -g -s -d line/ 11978
+$ tree line/
+line/
+└── Animated_POP_TEAM_EPIC_Sound_Stickers
+    ├── @2x_animation_png
+    │   ├── 67561910@2x.png
+    │   ├── 67561911@2x.png
+    │   ├── 67561912@2x.png
+    │   ├── 67561913@2x.png
+    │   ├── 67561914@2x.png
+    │   ├── 67561915@2x.png
+    │   ├── 67561916@2x.png
+    │   ├── 67561917@2x.png
+    │   ├── 67561918@2x.png
+    │   ├── 67561919@2x.png
+    │   ├── 67561920@2x.png
+    │   ├── 67561921@2x.png
+    │   ├── 67561922@2x.png
+    │   ├── 67561923@2x.png
+    │   ├── 67561924@2x.png
+    │   ├── 67561925@2x.png
+    │   ├── 67561926@2x.png
+    │   ├── 67561927@2x.png
+    │   ├── 67561928@2x.png
+    │   ├── 67561929@2x.png
+    │   ├── 67561930@2x.png
+    │   ├── 67561931@2x.png
+    │   ├── 67561932@2x.png
+    │   └── 67561933@2x.png
+    ├── @2x_gif
+    │   ├── 67561910@2x.gif
+    │   ├── 67561911@2x.gif
+    │   ├── 67561912@2x.gif
+    │   ├── 67561913@2x.gif
+    │   ├── 67561914@2x.gif
+    │   ├── 67561915@2x.gif
+    │   ├── 67561916@2x.gif
+    │   ├── 67561917@2x.gif
+    │   ├── 67561918@2x.gif
+    │   ├── 67561919@2x.gif
+    │   ├── 67561920@2x.gif
+    │   ├── 67561921@2x.gif
+    │   ├── 67561922@2x.gif
+    │   ├── 67561923@2x.gif
+    │   ├── 67561924@2x.gif
+    │   ├── 67561925@2x.gif
+    │   ├── 67561926@2x.gif
+    │   ├── 67561927@2x.gif
+    │   ├── 67561928@2x.gif
+    │   ├── 67561929@2x.gif
+    │   ├── 67561930@2x.gif
+    │   ├── 67561931@2x.gif
+    │   ├── 67561932@2x.gif
+    │   └── 67561933@2x.gif
+    ├── @2x_key_png
+    │   ├── 67561910@2x_key.png
+    │   ├── 67561911@2x_key.png
+    │   ├── 67561912@2x_key.png
+    │   ├── 67561913@2x_key.png
+    │   ├── 67561914@2x_key.png
+    │   ├── 67561915@2x_key.png
+    │   ├── 67561916@2x_key.png
+    │   ├── 67561917@2x_key.png
+    │   ├── 67561918@2x_key.png
+    │   ├── 67561919@2x_key.png
+    │   ├── 67561920@2x_key.png
+    │   ├── 67561921@2x_key.png
+    │   ├── 67561922@2x_key.png
+    │   ├── 67561923@2x_key.png
+    │   ├── 67561924@2x_key.png
+    │   ├── 67561925@2x_key.png
+    │   ├── 67561926@2x_key.png
+    │   ├── 67561927@2x_key.png
+    │   ├── 67561928@2x_key.png
+    │   ├── 67561929@2x_key.png
+    │   ├── 67561930@2x_key.png
+    │   ├── 67561931@2x_key.png
+    │   ├── 67561932@2x_key.png
+    │   └── 67561933@2x_key.png
+    ├── @2x_png
+    │   ├── 67561910@2x.png
+    │   ├── 67561911@2x.png
+    │   ├── 67561912@2x.png
+    │   ├── 67561913@2x.png
+    │   ├── 67561914@2x.png
+    │   ├── 67561915@2x.png
+    │   ├── 67561916@2x.png
+    │   ├── 67561917@2x.png
+    │   ├── 67561918@2x.png
+    │   ├── 67561919@2x.png
+    │   ├── 67561920@2x.png
+    │   ├── 67561921@2x.png
+    │   ├── 67561922@2x.png
+    │   ├── 67561923@2x.png
+    │   ├── 67561924@2x.png
+    │   ├── 67561925@2x.png
+    │   ├── 67561926@2x.png
+    │   ├── 67561927@2x.png
+    │   ├── 67561928@2x.png
+    │   ├── 67561929@2x.png
+    │   ├── 67561930@2x.png
+    │   ├── 67561931@2x.png
+    │   ├── 67561932@2x.png
+    │   └── 67561933@2x.png
+    ├── animation_png
+    │   ├── 67561910.png
+    │   ├── 67561911.png
+    │   ├── 67561912.png
+    │   ├── 67561913.png
+    │   ├── 67561914.png
+    │   ├── 67561915.png
+    │   ├── 67561916.png
+    │   ├── 67561917.png
+    │   ├── 67561918.png
+    │   ├── 67561919.png
+    │   ├── 67561920.png
+    │   ├── 67561921.png
+    │   ├── 67561922.png
+    │   ├── 67561923.png
+    │   ├── 67561924.png
+    │   ├── 67561925.png
+    │   ├── 67561926.png
+    │   ├── 67561927.png
+    │   ├── 67561928.png
+    │   ├── 67561929.png
+    │   ├── 67561930.png
+    │   ├── 67561931.png
+    │   ├── 67561932.png
+    │   └── 67561933.png
+    ├── gif
+    │   ├── 67561910.gif
+    │   ├── 67561911.gif
+    │   ├── 67561912.gif
+    │   ├── 67561913.gif
+    │   ├── 67561914.gif
+    │   ├── 67561915.gif
+    │   ├── 67561916.gif
+    │   ├── 67561917.gif
+    │   ├── 67561918.gif
+    │   ├── 67561919.gif
+    │   ├── 67561920.gif
+    │   ├── 67561921.gif
+    │   ├── 67561922.gif
+    │   ├── 67561923.gif
+    │   ├── 67561924.gif
+    │   ├── 67561925.gif
+    │   ├── 67561926.gif
+    │   ├── 67561927.gif
+    │   ├── 67561928.gif
+    │   ├── 67561929.gif
+    │   ├── 67561930.gif
+    │   ├── 67561931.gif
+    │   ├── 67561932.gif
+    │   └── 67561933.gif
+    ├── key_png
+    │   ├── 67561910_key.png
+    │   ├── 67561911_key.png
+    │   ├── 67561912_key.png
+    │   ├── 67561913_key.png
+    │   ├── 67561914_key.png
+    │   ├── 67561915_key.png
+    │   ├── 67561916_key.png
+    │   ├── 67561917_key.png
+    │   ├── 67561918_key.png
+    │   ├── 67561919_key.png
+    │   ├── 67561920_key.png
+    │   ├── 67561921_key.png
+    │   ├── 67561922_key.png
+    │   ├── 67561923_key.png
+    │   ├── 67561924_key.png
+    │   ├── 67561925_key.png
+    │   ├── 67561926_key.png
+    │   ├── 67561927_key.png
+    │   ├── 67561928_key.png
+    │   ├── 67561929_key.png
+    │   ├── 67561930_key.png
+    │   ├── 67561931_key.png
+    │   ├── 67561932_key.png
+    │   └── 67561933_key.png
+    ├── png
+    │   ├── 67561910.png
+    │   ├── 67561911.png
+    │   ├── 67561912.png
+    │   ├── 67561913.png
+    │   ├── 67561914.png
+    │   ├── 67561915.png
+    │   ├── 67561916.png
+    │   ├── 67561917.png
+    │   ├── 67561918.png
+    │   ├── 67561919.png
+    │   ├── 67561920.png
+    │   ├── 67561921.png
+    │   ├── 67561922.png
+    │   ├── 67561923.png
+    │   ├── 67561924.png
+    │   ├── 67561925.png
+    │   ├── 67561926.png
+    │   ├── 67561927.png
+    │   ├── 67561928.png
+    │   ├── 67561929.png
+    │   ├── 67561930.png
+    │   ├── 67561931.png
+    │   ├── 67561932.png
+    │   └── 67561933.png
+    └── sound
+        ├── 67561910.m4a
+        ├── 67561911.m4a
+        ├── 67561912.m4a
+        ├── 67561913.m4a
+        ├── 67561914.m4a
+        ├── 67561915.m4a
+        ├── 67561916.m4a
+        ├── 67561917.m4a
+        ├── 67561918.m4a
+        ├── 67561919.m4a
+        ├── 67561920.m4a
+        ├── 67561921.m4a
+        ├── 67561922.m4a
+        ├── 67561923.m4a
+        ├── 67561924.m4a
+        ├── 67561925.m4a
+        ├── 67561926.m4a
+        ├── 67561927.m4a
+        ├── 67561928.m4a
+        ├── 67561929.m4a
+        ├── 67561930.m4a
+        ├── 67561931.m4a
+        ├── 67561932.m4a
+        └── 67561933.m4a
+
+10 directories, 216 files
+
+```
+### LIcense
+This source code is licensed MIT.
---
+++ src/main.ts
@@ -0,0 +1,227 @@
+const req = require("request");
+const fs = require("fs-extra");
+const program = require("commander");
+
+program
+  .version("1.0.0")
+  .usage("[options] [sticker_id]")
+  .option("-a, --animation", "With animation stickers (APNG)")
+  .option("-g, --gif", "With animation stickers (GIF)")
+  .option("-s, --sound", "With sound(animation) stickers sound (m4a)")
+  .option(
+    "-d --dir <dir>",
+    "Specify the directory where you want to store the data"
+  )
+  .parse(process.argv);
+
+const sticker_id = program.args;
+const url = `http://dl.stickershop.line.naver.jp/products/0/0/1/${sticker_id}/iphone/productInfo.meta`;
+
+req(url, (err: string, body) => {
+  if (err !== null) {
+    console.error("error:", err);
+    return false;
+  }
+
+  //Extract the ID and name of the sticker
+  const get_json = JSON.parse(body.body);
+  const title_en: string = get_json.title["en"].replace(/ /g, "_");
+  const stickers_obj: string[] = get_json.stickers;
+  const stickers_id: string[] = [];
+  for (let i = 0; i < stickers_obj.length; i++) {
+    stickers_id[i] = stickers_obj[i]["id"];
+  }
+
+  //Creating a storage directory
+  let png_dir: string;
+  let _2x_png_dir: string;
+  let key_png_dir: string;
+  let _2x_key_png_dir: string;
+  if (program.dir) {
+    png_dir = `${program.dir}/${title_en}/png`;
+    _2x_png_dir = `${program.dir}/${title_en}/@2x_png`;
+    key_png_dir = `${program.dir}/${title_en}/key_png`;
+    _2x_key_png_dir = `${program.dir}/${title_en}/@2x_key_png`;
+  } else {
+    png_dir = `./${title_en}/png`;
+    _2x_png_dir = `./${title_en}/@2x_png`;
+    key_png_dir = `./${title_en}/key_png`;
+    _2x_key_png_dir = `./${title_en}/@2x_key_png`;
+  }
+
+  fs.mkdirs(png_dir, (err: string) => {
+    if (err) return console.error(err);
+  });
+  fs.mkdirs(_2x_png_dir, (err: string) => {
+    if (err) return console.error(err);
+  });
+  fs.mkdirs(key_png_dir, (err: string) => {
+    if (err) return console.error(err);
+  });
+  fs.mkdirs(_2x_key_png_dir, (err: string) => {
+    if (err) return console.error(err);
+  });
+
+  //Download the sticker from the extracted ID and save it
+  for (let i = 0; i < stickers_id.length; i++) {
+    const id: string = stickers_id[i];
+    const png_url = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/sticker.png`;
+    const _2x_png_url = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/sticker@2x.png`;
+    const key_png_url = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/sticker_key.png`;
+    const _2x_key_png_url = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/sticker_key@2x.png`;
+
+    req(
+      { method: "GET", url: png_url, encoding: null },
+      (err: string, res, body) => {
+        if (!err && res.statusCode === 200) {
+          fs.writeFileSync(`${png_dir}/${id}.png`, body, "binary");
+        }
+      }
+    );
+    req(
+      { method: "GET", url: _2x_png_url, encoding: null },
+      (err: string, res, body) => {
+        if (!err && res.statusCode === 200) {
+          fs.writeFileSync(`${_2x_png_dir}/${id}@2x.png`, body, "binary");
+        }
+      }
+    );
+    req(
+      { method: "GET", url: key_png_url, encoding: null },
+      (err: string, res, body) => {
+        if (!err && res.statusCode === 200) {
+          fs.writeFileSync(`${key_png_dir}/${id}_key.png`, body, "binary");
+        }
+      }
+    );
+    req(
+      { method: "GET", url: _2x_key_png_url, encoding: null },
+      (err: string, res, body) => {
+        if (!err && res.statusCode === 200) {
+          fs.writeFileSync(
+            `${_2x_key_png_dir}/${id}@2x_key.png`,
+            body,
+            "binary"
+          );
+        }
+      }
+    );
+  }
+
+  //For animation(apng)
+  if (program.animation) {
+    for (let i = 0; i < stickers_id.length; i++) {
+      const id: string = stickers_id[i];
+
+      let a_png_dir: string;
+      let _2x_a_png_dir: string;
+      if (program.dir) {
+        a_png_dir = `${program.dir}/${title_en}/animation_png`;
+        _2x_a_png_dir = `${program.dir}/${title_en}/@2x_animation_png`;
+      } else {
+        a_png_dir = `./${title_en}/animation_png`;
+        _2x_a_png_dir = `./${title_en}/@2x_animation_png`;
+      }
+
+      const a_png_url = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/sticker_animation.png`;
+      const _2x_a_png_url = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/sticker_animation@2x.png`;
+
+      fs.mkdirs(a_png_dir, (err: string) => {
+        if (err) return console.error(err);
+      });
+      fs.mkdirs(_2x_a_png_dir, (err: string) => {
+        if (err) return console.error(err);
+      });
+
+      req(
+        { method: "GET", url: a_png_url, encoding: null },
+        (err: string, res, body) => {
+          if (!err && res.statusCode === 200) {
+            fs.writeFileSync(`${a_png_dir}/${id}.png`, body, "binary");
+          }
+        }
+      );
+      req(
+        { method: "GET", url: _2x_a_png_url, encoding: null },
+        (err: string, res, body) => {
+          if (!err && res.statusCode === 200) {
+            fs.writeFileSync(`${_2x_a_png_dir}/${id}@2x.png`, body, "binary");
+          }
+        }
+      );
+    }
+  }
+
+  //For animation(gif)
+  if (program.gif) {
+    for (let i = 0; i < stickers_id.length; i++) {
+      const id: string = stickers_id[i];
+
+      let gif_dir: string;
+      let _2x_gif_dir: string;
+      if (program.dir) {
+        gif_dir = `${program.dir}/${title_en}/gif`;
+        _2x_gif_dir = `${program.dir}/${title_en}/@2x_gif`;
+      } else {
+        gif_dir = `./${title_en}/gif`;
+        _2x_gif_dir = `./${title_en}/@2x_gif`;
+      }
+
+      const gif_url = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/sticker_animation.png`;
+      const _2x_gif_url = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/sticker_animation@2x.png`;
+
+      fs.mkdirs(gif_dir, (err: string) => {
+        if (err) return console.error(err);
+      });
+      fs.mkdirs(_2x_gif_dir, (err: string) => {
+        if (err) return console.error(err);
+      });
+
+      req(
+        { method: "GET", url: gif_url, encoding: null },
+        (err: string, res, body) => {
+          if (!err && res.statusCode === 200) {
+            fs.writeFileSync(`${gif_dir}/${id}.gif`, body, "binary");
+          }
+        }
+      );
+      req(
+        { method: "GET", url: _2x_gif_url, encoding: null },
+        (err: string, res, body) => {
+          if (!err && res.statusCode === 200) {
+            fs.writeFileSync(`${_2x_gif_dir}/${id}@2x.gif`, body, "binary");
+          }
+        }
+      );
+    }
+  }
+
+  //For sound
+  if (program.sound) {
+    for (let i = 0; i < stickers_id.length; i++) {
+      const id: string = stickers_id[i];
+
+      let sound_dir: string;
+      if (program.dir) {
+        sound_dir = `${program.dir}/${title_en}/sound`;
+      } else {
+        sound_dir = `./${title_en}/sound`;
+      }
+
+      const sound_url = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/android/sticker_sound.m4a`;
+
+      fs.mkdirs(sound_dir, (err: string) => {
+        if (err) return console.error(err);
+      });
+
+      req(
+        { method: "GET", url: sound_url, encoding: null },
+        (err: string, res, body) => {
+          if (!err && res.statusCode === 200) {
+            fs.writeFileSync(`${sound_dir}/${id}.m4a`, body, "binary");
+          }
+        }
+      );
+    }
+  }
+});
