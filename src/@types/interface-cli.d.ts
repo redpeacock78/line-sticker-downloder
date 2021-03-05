d658bc192ee12f4186af018137d75f97eb5e37f8
redpeacock78 <scarletpeacock78@gmail.com>
2021-03-05 16:37:47 +0900

    Add content: src/@types/interface-cli.d.ts

---
+++ src/@types/interface-cli.d.ts
@@ -0,0 +1,11 @@
+import commander = require("commander");
+
+export interface InterfaceCLI extends commander.Command {
+  dir?: string;
+  gif?: string;
+  manga?: string;
+  effect?: string;
+  sound?: string;
+  custom?: string;
+  animation?: string;
+}
