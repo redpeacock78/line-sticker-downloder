f352c3a2aade1bbcb77c86849cc63c5a0420a7c4
redpeacock78 <scarletpeacock78@gmail.com>
2021-03-05 15:08:46 +0900

    Add content: release.sh

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# check this version is enable to release or not
+npx can-npm-publish
+if [[ "${?}" == 1 ]]; then
+    exit 255
+fi
+
+# get current version from package.json
+TAG="$(grep version <package.json | cut -d ' ' -f 4 | tr -d ',' | tr -d \")"
+echo "add new tag to GitHub: ${TAG}"
+
+# Add tag to GitHub
+API_URL="https://api.github.com/repos/${REPO}/git/refs"
+
+curl -s -X POST "${API_URL}" \
+    -H "Authorization: token ${GITHUB_TOKEN}" \
+    -d @- <<EOS
+{
+  "ref": "refs/tags/${TAG}",
+  "sha": "${COMMIT}"
+}
+EOS
