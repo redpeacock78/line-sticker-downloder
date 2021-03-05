ee7fd4b8ebb7e7e85fa49a911d8d7c5fae42ebc5
redpeacock78 <scarletpeacock78@gmail.com>
2021-03-05 15:36:20 +0900

    Change content: release.sh

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # check this version is enable to release or not
-npx -y -p can-npm-publish can-npm-publish
+yarn run can-npm-publish
 if [[ "${?}" == 1 ]]; then
     exit 255
 fi
