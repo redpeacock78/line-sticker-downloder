9f91d249a0bea67b41b3b56a143c4fba7a070c06
redpeacock78 <scarletpeacock78@gmail.com>
2021-03-07 01:51:57 +0900

    Change content: src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,18 @@ Copyright (c) 2020 redpeacock78
 This software is released under the MIT License, see LICENSE.
 */
 
+import * as req from "request";
 import * as fs from "fs-extra";
+import { Command } from 'commander';
 import axios, { AxiosResponse } from "axios";
 import * as json from "./@types/sticker-json";
 import { apng2gif } from "./@types/apng2gif";
-import { InterfaceCLI } from "./@types/interface-cli"
-const program: InterfaceCLI = require("commander");
 const apng2gif: apng2gif = require("apng2gif");
+const packageJson = require("../package.json")
 
+const program = new Command();
 program
-  .version("1.2.10", "-v, --version", "Output the version number")
+  .version(packageJson.version, "-v, --version", "Output the version number")
   .name("lsdl")
   .usage("[options] [sticker_id]")
   .description("Download the data extracted from the LINE sticker.")
@@ -47,6 +49,8 @@ Example:
   })
   .parse(process.argv);
 
+const options = program.opts();
+
 const sticker_id = Number(
   program.args[0].slice(0, 42) !== "https://store.line.me/stickershop/product/"
     ? program.args[0]
@@ -66,10 +70,16 @@ const mkdir = (dir_neme: string): void => {
 };
 
 const image_dl = async (img_url: string, png: string, gif?: string): Promise<void> => {
-  const res: AxiosResponse<Buffer> = await axios.get(img_url, { responseType: 'arraybuffer' });
-  fs.writeFile(png, Buffer.from(res.data), "binary").then((): void => {
-    !gif ? "" : apng2gif(png, gif);
-  })
+  req(
+    { method: "GET", url: img_url, encoding: null },
+    (err: string, res: req.Response, body: req.RequestCallback): void => {
+      !err && res.statusCode === 200
+        ? fs.writeFile(png, body, "binary").then((): void => {
+          !gif ? "" : apng2gif(png, gif);
+        })
+        : "";
+    }
+  );
 };
 
 const main = async (url: string): Promise<boolean> => {
@@ -87,17 +97,22 @@ const main = async (url: string): Promise<boolean> => {
     }, []);
 
     stickers_id.filter((id: number): void => {
-      const dir_name = `${program.dir}/${title_en}`;
-      const png_dir: string = program.dir
+      // @ts-ignore
+      const dir_name = `${options.dir}/${title_en}`;
+      // @ts-ignore
+      const png_dir: string = options.dir
         ? `${dir_name}/png`
         : `./${title_en}/png`;
-      const _2x_png_dir: string = program.dir
+      // @ts-ignore
+      const _2x_png_dir: string = options.dir
         ? `${dir_name}/@2x_png`
         : `./${title_en}/@2x_png`;
-      const key_png_dir: string = program.dir
+      // @ts-ignore
+      const key_png_dir: string = options.dir
         ? `${dir_name}/key_png`
         : `./${title_en}/key_png`;
-      const _2x_key_png_dir: string = program.dir
+      // @ts-ignore
+      const _2x_key_png_dir: string = options.dir
         ? `${dir_name}/@2x_key_png`
         : `./${title_en}/@2x_key_png`;
       const dir_names: string[] = [
@@ -106,24 +121,32 @@ const main = async (url: string): Promise<boolean> => {
         key_png_dir,
         _2x_key_png_dir,
       ];
-      const png_url: string = program.custom
+      // @ts-ignore
+      const png_url: string = options.custom
         ? `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/base/sticker.png`
-        : program.manga
+        // @ts-ignore
+        : options.manga
           ? `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/base/plus/sticker.png`
           : `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/sticker.png`;
-      const _2x_png_url: string = program.custom
+      // @ts-ignore
+      const _2x_png_url: string = options.custom
         ? `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/base/sticker@2x.png`
-        : program.manga
+        // @ts-ignore
+        : options.manga
           ? `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/base/plus/sticker@2x.png`
           : `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/sticker@2x.png`;
-      const key_png_url: string = program.custom
+      // @ts-ignore
+      const key_png_url: string = options.custom
         ? `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/base/sticker_key.png`
-        : program.manga
+        // @ts-ignore
+        : options.manga
           ? `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/base/plus/sticker_key.png`
           : `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/sticker_key.png`;
-      const _2x_key_png_url: string = program.custom
+      // @ts-ignore
+      const _2x_key_png_url: string = options.custom
         ? `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/base/sticker_key@2x.png`
-        : program.manga
+        // @ts-ignore
+        : options.manga
           ? `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/base/plus/sticker_key@2x.png`
           : `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/iPhone/sticker_key@2x.png`;
       Promise.resolve()
@@ -144,27 +167,37 @@ const main = async (url: string): Promise<boolean> => {
     });
 
     //For animation
-    program.animation
+    // @ts-ignore
+    options.animation
       ? stickers_id.filter((id: number): void => {
-        const dir_name: string = program.dir
-          ? `${program.dir}/${title_en}`
+        // @ts-ignore
+        const dir_name: string = options.dir
+          // @ts-ignore
+          ? `${options.dir}/${title_en}`
           : "";
-        const a_png_dir: string = program.dir
+        // @ts-ignore
+        const a_png_dir: string = options.dir
+          // @ts-ignore
           ? `${dir_name}/animation_png`
           : `./${title_en}/animation_png`;
-        const _2x_a_png_dir: string = program.dir
+        // @ts-ignore
+        const _2x_a_png_dir: string = options.dir
           ? `${dir_name}/@2x_animation_png`
           : `./${title_en}/@2x_animation_png`;
         const gif_dir: string =
-          program.dir && program.gif
+          // @ts-ignore
+          options.dir && options.gif
             ? `${dir_name}/gif`
-            : program.gif
+            // @ts-ignore
+            : options.gif
               ? `./${title_en}/gif`
               : "";
         const _2x_gif_dir: string =
-          program.dir && program.gif
+          // @ts-ignore
+          options.dir && options.gif
             ? `${dir_name}/@2x_gif`
-            : program.gif
+            // @ts-ignore
+            : options.gif
               ? `./${title_en}/@2x_gif`
               : "";
         const a_png_dirs: string[] = [a_png_dir, _2x_a_png_dir];
@@ -176,21 +209,24 @@ const main = async (url: string): Promise<boolean> => {
             a_png_dirs.filter((v: string): void => {
               mkdir(v);
             });
-            program.gif
+            // @ts-ignore
+            options.gif
               ? gif_dirs.filter((v: string): void => {
                 mkdir(v);
               })
               : "";
           })
           .then((): void => {
-            program.gif
+            // @ts-ignore
+            options.gif
               ? image_dl(
                 a_png_url,
                 `${a_png_dir}/${id}.png`,
                 `${gif_dir}/${id}.gif`
               )
               : image_dl(a_png_url, `${a_png_dir}/${id}.png`);
-            program.gif
+            // @ts-ignore
+            options.gif
               ? image_dl(
                 _2x_a_png_url,
                 `${_2x_a_png_dir}/${id}@2x.png`,
@@ -205,26 +241,33 @@ const main = async (url: string): Promise<boolean> => {
       : "";
 
     //For Effect
-    program.effect
+    // @ts-ignore
+    options.effect
       ? stickers_id.filter((id: number): void => {
-        const dir_name = `${program.dir}/${title_en}`;
-        const e_png_dir: string = program.dir
+        // @ts-ignore
+        const dir_name = `${options.dir}/${title_en}`;
+        // @ts-ignore
+        const e_png_dir: string = options.dir
           ? `${dir_name}/effect_png`
           : `./${title_en}/effect_png`;
         const gif_dir: string =
-          program.dir && program.gif
+          // @ts-ignore
+          options.dir && options.gif
             ? `${dir_name}/effect_gif`
-            : program.gif
+            // @ts-ignore
+            : options.gif
               ? `./${title_en}/effect_gif`
               : "";
         const e_png_url = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/android/sticker_popup.png`;
         Promise.resolve()
           .then((): void => {
             mkdir(e_png_dir);
-            program.gif ? mkdir(gif_dir) : "";
+            // @ts-ignore
+            options.gif ? mkdir(gif_dir) : "";
           })
           .then((): void => {
-            program.gif
+            // @ts-ignore
+            options.gif
               ? image_dl(
                 e_png_url,
                 `${e_png_dir}/${id}.png`,
@@ -236,10 +279,13 @@ const main = async (url: string): Promise<boolean> => {
       : "";
 
     //For sound
-    program.sound
+    // @ts-ignore
+    options.sound
       ? stickers_id.filter((id: number): void => {
-        const sound_dir: string = program.dir
-          ? `${program.dir}/${title_en}/sound`
+        // @ts-ignore
+        const sound_dir: string = options.dir
+          // @ts-ignore
+          ? `${options.dir}/${title_en}/sound`
           : `./${title_en}/sound`;
         const sound_url = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/android/sticker_sound.m4a`;
         Promise.resolve()
